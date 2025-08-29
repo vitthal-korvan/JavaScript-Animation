6ba503913ce2d898d25c5a6954025a4b37340451
Vitthal Korvan <korvanvitthal@gmail.com>
2025-08-29 13:19:13 +0530

    portfolio using gsap added!

---
+++ GSAP Portfolio Basic/script.js
@@ -0,0 +1,30 @@
+let tl = gsap.timeline();
+tl.from("#nav h3", {
+  y: -50,
+  opacity: 0,
+  delay: 0.4,
+  duration: 0.8,
+  stagger: 0.2,
+});
+
+tl.from("#main h1", {
+  x: -500,
+  opacity: 0,
+  duration: 0.8,
+  stagger: 0.2,
+});
+
+tl.from("img", {
+  x: 100,
+  rotate: 45,
+  opacity: 0,
+  duration: 0.8,
+  stagger: 0.5,
+});
+
+tl.from("h2", {
+  x: -500,
+  opacity: 0,
+  duration: 0.8,
+  stagger: 0.2,
+});
